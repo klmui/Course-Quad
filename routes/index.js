4cafee3a288fadf937f9751ec9f831240af26b17
Kenneth Mui <klmui@wisc.edu>
2020-07-19 04:24:01 -0500

    updates to UI and finish mock-up for instructors page

--- routes/index.js
+++ routes/index.js
@@ -20,27 +20,35 @@ router.get('/', function(req, res) {
 // POST login info  
 router.post('/login', function(req, res) {
   // get the username and password from the request (comes from the name attr of the input)
-  var user = {"username": req.body.username, "password": req.body.password};
-  console.log("username:" + user.username + ", password: " + "secret");
-
-  // render the home page and change the name to the email
-  res.render('index', {email: user.username});
+  connection.query("SELECT * FROM Course", function(error, rows, fields) {
+    if (error) {
+      console.log('Error in test query');
+    } else {
+      var user = {"username": req.body.username, "password": req.body.password};
+      console.log("username:" + user.username + ", password: " + "secret");
+    
+      // render the home page and change the name to the email
+      res.render('index', {email: user.username, 'courses': rows});
+    }
+  });
 });
 
 // POST sign up info
 router.post('/signup', function(req, res){
-  // get the username and password from the request
-  var newUser = {"username": req.body.username, "password": req.body.password};
+  connection.query("SELECT * FROM Course", function(error, rows, fields) {
+    if (error) {
+      console.log('Error in test query');
+    } else {
+       // get the username and password from the request
+      var newUser = {"username": req.body.username, "password": req.body.password};
 
-  console.log("username:" + newUser.username + ", password: " + "secret");
+      console.log("username:" + newUser.username + ", password: " + "secret");
 
-  //TODO check that the username is unique (i.e., not already in the database)
-  res.render('index', {email: newUser.username});
-});
+      res.render('index', {email: newUser.username, 'courses': rows});
 
-// GET (render) signup page
-router.get('/signup', function(req, res) {
-  res.render('signup');
+      //TODO check that the username is unique (i.e., not already in the database)
+    }
+  });
 });
 
 module.exports = router;
--- routes/instructors.js
+++ routes/instructors.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 // show all instructors
 router.get('/', function(req, res) {
-  res.render('instructors/instructors');
+  connection.query("SELECT * FROM Instructor", function(error, rows, fields) {
+    if (error) {
+      console.log('Error in test query');
+    } else {
+      res.render('instructors/instructors', {"instructors": rows});
+      console.log(rows);
+    }
+  });
 });
 
 // show a particular instructor
