69d11134e6cffd6775e22c85eb5ef394eac43593
Kenneth Mui <klmui@wisc.edu>
2020-07-23 03:47:15 -0500

    finish explore study groups page except advanced search

--- routes/studyGroups.js
+++ routes/studyGroups.js
@@ -8,8 +8,29 @@ router.get('/', authController.isLoggedIn, function(req, res) {
 });
 
 // explore study groups
-router.get('/explore', authController.isLoggedIn, function(req, res){
-  res.render('studyGroups/explore', {'user': req.user});
+router.get('/explore', authController.isLoggedIn, function(req, res) {
+  var query = `
+    SELECT * FROM StudyGroup;
+  `;
+  connection.query(query, function(error, rows, fields) {
+    if (error) {
+      console.log("error in query");
+    } else {
+      var query2 = `
+        SELECT s.ID, u.email, u.username, m.sort_order FROM StudyGroup s
+        JOIN Membership m on s.ID=m.study_group_id
+        JOIN User u on m.username=u.username
+        ORDER BY s.ID, m.sort_order;
+      `;
+      connection.query(query2, function(error, rows2, fields) {
+        if (error) {
+          console.log("error in query");
+        } else {
+          res.render('studyGroups/explore', {'user': req.user, 'studyGroups': rows, 'users': rows2});
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
